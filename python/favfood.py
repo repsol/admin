7ec72c11acdd21f6b8c5e47b4cd86f2a91b31e1f
Repsol <repsol@gmail.com>
2013-09-04 11:09:48 -0700

    Adding Python to the repo

---
+++ python/favfood.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#What is your favorite food
+
+name = raw_input("Hello, what's your name? ")
+print "Hi ", name
+
+ent = raw_input("What is your favorite food? ")
+side = raw_input("What is your second favorite food? ")
+
+print name, "your favorite food is", ent+side
+
+
+
