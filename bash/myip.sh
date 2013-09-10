a5bfbb0e3a19f44b6ddab540de8e7ebbb224e5b4
Repsol <repsol@gmail.com>
2013-09-10 15:46:54 -0700

    changed the website where I get my IP

--- bash/myip.sh
+++ bash/myip.sh
@@ -5,28 +5,30 @@
 #it changes send me an email with the new IP 
 
 #Declare variables
-text='/home/admin/text/myip.txt'
+text='/admin/text/myip.txt'
 EMAIL=azexptech@gmail.com
 
 
 #This script will get my public IP then email it to me at a GMAIL account
-IP=`wget http://findmyip.org -O - -o /dev/null | grep '<span class="title">Your' |awk '{print $6}' |cut -d "<" -f 1`
+#IP=`wget http://findmyip.org -O - -o /dev/null | grep '<span class="title">Your' |awk '{print $6}' |cut -d "<" -f 1`
 
+IP=$( curl icanhazip.com )
 #Test
-#echo $IP
+echo $IP
 
 #Check the $text list for the last IP on the list
 tres=`cat $text |tail -n 1`
 
 #Test
-#echo $tres
+newtres=$( echo -n "${tres}" )
+echo "${newtres}"
 
 #Here we test that the last IP on the list in $text is 
 #the same or different than the actual public IP
 #If it is not equal it appends the new IP to the list and
 #sends an email with the new IP
 
-	if [ "$IP" != "$tres" ]
+	if [ "$IP" != "$newtres" ]
 	then
 		echo $IP >> $text
 		BODY="Your IP address has been changed to $IP!"
