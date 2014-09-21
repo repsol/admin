dc4dc2ac4d2436c60a09a9dbb831db4a5d4cc5f2
Repsol <repsol05@gmail.com>
2014-09-21 01:35:53 -0700

    Adding the script that monitors the Trivia daemon

---
+++ bash/trivch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# This script watches the trivia process and restarts it if it dies
+# Written by Mike Addicks 9/19/14
+#
+#
+
+cd /opt/trivi_lite-0.2.3/
+daemon=$( ps -ef |grep triv[i] )
+
+if [ "${daemon##*\/}" != 'trivi_lite.pl' ] ; then
+
+	./trivi_lite.pl &
+fi
